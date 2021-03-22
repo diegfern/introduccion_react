04089a6038067395b9a225814b9a469dc42d268d
diegfern <81177622+diegfern@users.noreply.github.com>
2021-03-22 19:46:29 -0300

    Se optimizó handleClick con un if sentence

--- src/Counter.js
+++ src/Counter.js
@@ -3,19 +3,17 @@ import React, { useState, Fragment } from "react";
 const Counter = () => {
   const [count, setCount] = useState(0);
 
-  const handleClickAdd = () => {
-    setCount(count + 1);
-  };
-  const handleClickSub = () => {
-    setCount(count - 1);
+  const handleClick = (bool) => {
+    if (bool) setCount(count + 1);
+    else setCount(count - 1);
   };
 
   return (
     <Fragment>
-      <button className="btn" onClick={() => handleClickAdd()}>
+      <button className="btn" onClick={() => handleClick(true)}>
         +1
       </button>
-      <button className="btn" onClick={() => handleClickSub()}>
+      <button className="btn" onClick={() => handleClick(false)}>
         -1
       </button>
       <p>Nuestro contador es {count}</p>
